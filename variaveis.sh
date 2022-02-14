f69d1b742a77a7e9a80e0adbd520c37aa5fc30e3
Israel Passos <israelpassoss36@gmail.com>
2022-02-13 21:35:52 -0300

    Exemplos de variáveis resevadas

--- variaveis.sh
+++ variaveis.sh
@@ -8,4 +8,27 @@ ANO_ATUAL=2021
 
 IDADE=$((ANO_ATUAL-ANO_NASCIMENTO))
 
-echo $IDADE
+echo "$IDADE"
+
+NUMERO_1=25
+NUMERO_2=25
+
+TOTAL=$(($NUMERO_1+$NUMERO_2))
+
+echo $TOTAL
+
+echo "---------------------------------------------"
+
+echo "Parametro: 1 $1"
+
+echo "Parametro: 2 $2"
+
+echo "Todos os Paramentros: $*"
+
+echo "Quantos Parametros: $#"
+
+echo "Saída do Último Comando: $?"
+
+echo "O número do processo excutando: PID $$"
+
+echo "Buscando o nome do script que estamos excutando: $0"
